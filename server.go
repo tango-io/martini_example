8895335db781385eca8f02fae205e2bfd3841be4
Antonio Chavez <cavjzz@gmail.com>
2014-07-30 11:07:34 -0500

    Added update persons

--- server.go
+++ server.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-  "fmt"
+  _ "fmt"
   "github.com/go-martini/martini"
   "github.com/martini-contrib/render"
   "database/sql"
@@ -50,17 +50,34 @@ func newPerson(r render.Render) {
 }
 
 func createPerson(r render.Render, req *http.Request, db *sql.DB){
-  fmt.Println(req.FormValue("person"))
   _, err := db.Query("INSERT INTO persons (id, name, age, email, job) VALUES (DEFAULT, $1, $2, $3, $4)", req.FormValue("person[name]"), req.FormValue("person[age]"), req.FormValue("person[email]"), req.FormValue("person[job]"))
+	PanicIf(err)
+  r.Redirect("/")
+}
+
+func editPerson(r render.Render, params martini.Params, db *sql.DB) {
+  rows, err := db.Query("SELECT * FROM persons WHERE id = $1", params["id"])
+	PanicIf(err)
+  defer rows.Close()
+  person := Person{}
+
+  for rows.Next() {
+    err := rows.Scan(&person.Id, &person.Name, &person.Age, &person.Job, &person.Email)
+    PanicIf(err)
+  }
+
+  r.HTML(200, "persons/edit", person)
+}
+
+func updatePerson(r render.Render, req *http.Request, params martini.Params, db *sql.DB){
+  _, err := db.Query("UPDATE persons SET name = $1, age = $2, email = $3, job = $4 WHERE id = $5", req.FormValue("person[name]"), req.FormValue("person[age]"), req.FormValue("person[email]"), req.FormValue("person[job]"), params["id"])
 	PanicIf(err)
   r.Redirect("/")
 }
 
 func main() {
   m := martini.Classic()
-
   m.Map(SetupDB())
-
   m.Use(render.Renderer(render.Options {
     Directory: "views",
     Layout: "layouts/layout",
@@ -70,6 +87,8 @@ func main() {
   }))
 
   m.Get("/persons/new", newPerson)
+  m.Get("/persons/:id/edit", editPerson)
+  m.Post("/persons/:id", updatePerson)
   m.Post("/persons", createPerson)
   m.Get("/", index)
   m.Run()
