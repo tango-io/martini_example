61a356b65bee1ed65ded5139b35b207c9b7dbde5
Antonio Chavez <cavjzz@gmail.com>
2014-08-06 21:58:12 -0500

    Removed uneccesary SQL file

--- martini_example.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE persons (
-  id    serial PRIMARY KEY,
-  name  varchar(160),
-  age   integer,
-  job   varchar(160),
-  email varchar(160)
-);
-
-INSERT INTO persons VALUES (DEFAULT, 'Jeaneth Farmer', 23, 'TangoSource Engineer', 'xaid27@gmail.com');
